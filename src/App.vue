e386dd3e9bed23dbed67e84a9be9c3ce96ad7818
Hironori <hchapam@gmail.com>
2019-04-01 16:52:47 +0900

    first commit

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+  
+    <Header />
+    <router-view />
+
+
+  </div>
+      
+</template>
+
+<script>
+import Header from './components/layout/Header';
+export default{
+  name:"app",
+  components:{
+    Header
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+*{
+  box-sizing: border-box;
+  margin:0;
+  padding: 0;
+}
+</style>
---
+++ src/components/TodoItem.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class='todo-item' v-bind:class="{'is-complete':todo.completed}">
+<p>
+  <input type="checkbox" v-on:change="markComplete" name="" id="">
+   {{todo.title}}
+  <button @click="$emit('del-todo',todo.id)" class="del">x</button>
+   </p>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TodoItem',
+  props: ["todo"],
+  methods:{
+    markComplete(){
+      this.todo.completed = !this.todo.completed;
+    }
+
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+.todo-item{
+  background:#f4f4f4;
+  padding:10px;
+  border-bottom:1px;
+}
+
+.is-complete{
+  text-decoration: line-through;
+}
+
+.del{
+  background:#ff0000;
+}
+
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
